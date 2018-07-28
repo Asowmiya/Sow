c45a56181fce16fc7cdaa4c6c7260001dc2decc8
Asowmiya <41792340+Asowmiya@users.noreply.github.com>
2018-07-28 14:04:37 +0530

    Create Ammaaa.java

---
+++ Ammaaa.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ammaaa {
+	public static void main(String args[]) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the values:");
+		int i=in.nextInt();
+		if(i==0) {
+			System.out.println("0");
+		}
+		else if(i>0) {
+			System.out.println("positive");
+		}else {
+		System.out.println("negative");
+	}
+
+}
+}
